c015c23b3eefa543ce97ee2aed422ea74815476f
Keirryson Matheus <keirrysonmatheusrodriguez@gmail.com>
2025-07-16 16:13:57 -0300

    Adding (in progress) function to convert unties to fahrenheit and celsius

--- js/conversion.js
+++ js/conversion.js
@@ -1,12 +1,50 @@
 import { temperature , feelsLike } from "./ui.js";
-function convertToF(temp, element) {
+
+
+function convertUnity(temp, element , btn  , unity) {
   let Fahrenheit = Math.ceil(temp * 1.8 + 32)
-  if (element == 'temp') {
+   let Celsius = Math.ceil((temp - 32) / 1.8)
+  
+   if (element == 'temp' ) {
+    if( unity == 'Celsius'){
     temperature.textContent = Fahrenheit + 'F°';
+    btn.textContent = 'Converter para Celsius'
+    tempUnity = 'Fahrenheit'
+    }
+    else{
+    temperature.textContent = Celsius + 'C°';
+    btn.textContent = 'Converter para Fahrenheit'
+    unity = 'Celsius'
+    }
   }
   if (element == 'feelsLike') {
+      if( feelsLikeUnity === 'Celsius'){
     feelsLike.textContent = Fahrenheit + 'F°';
+    btn.textContent = 'Converter para Celsius'
+    unity = 'Fahrenheit'
+      }
+      else{
+    feelsLike.textContent = Celsius + 'C°';
+    btn.textContent = 'Converter para Fahrenheit'
+    unity = 'Fahrenheit'
+      }
   }
 }
 
-export { convertToF };
+// function convertToC (temp , element , btn){
+//   let Celsius = Math.ceil((temp - 32) / 1.8)
+//     if (element == 'temp') {
+//     temperature.textContent = Celsius + 'C°';
+//     btn.textContent = 'Converter para Fahrenheit'
+//     btn.onclick = convertToF( Fahrenheit , 'temp' , tempConvert )
+//   }
+//   if (element == 'feelsLike') {
+//     feelsLike.textContent = Celsius + 'C°';
+//     btn.textContent = 'Converter para Celsius'
+//     btn.onclick = convertToF( Fahrenheit , 'feelsLike' ,  feelsLikeConvert)
+//   }
+// }
+
+
+export { convertUnity };
+
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 
-import { convertToF } from './conversion.js';
+import { convertUnity } from './conversion.js';
 import { getDataByName, getDataByCoord , globalData } from './api.js';
 import { changeTheme, initTheme } from './theme.js';
 import { getPos }  from './currentPosition.js';
 
-
+let feelsLikeUnity = 'Celsius'
+let tempUnity = 'Celsius'
 const feelsLikeConvert = document.querySelector('#feelsLikeConvert')
 const tempConvert = document.querySelector('#tempToFah')
 const inputCity = document.querySelector('#findCity');
@@ -27,9 +28,9 @@ document.addEventListener('keydown', (event) => {
 
 themeBtn.addEventListener('click', changeTheme);
 tempConvert.addEventListener('click', () => {
-  convertToF(Math.ceil(globalData.main.temp), 'temp');
+  convertUnity(Math.ceil(globalData.main.temp), 'temp' , tempConvert , tempUnity );
 });
 
 feelsLikeConvert.addEventListener('click', () => {
-  convertToF(Math.ceil(globalData.main.feels_like), 'feelsLike', );
+  convertUnity(Math.ceil(globalData.main.feels_like), 'feelsLike', feelsLikeConvert , feelsLikeUnity );
 });
